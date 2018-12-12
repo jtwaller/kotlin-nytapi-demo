3dcdb769ce757f87dacc8935e6734d1f04d4c71c
Jack Waller <ucdwaller@gmail.com>
2018-12-12 13:39:28 -0800

    Add api models

---
+++ app/src/main/java/com/jtwaller/nytarticlesearchdemo/api/ApiModels.kt
@@ -0,0 +1,29 @@
+package com.jtwaller.nytarticlesearchdemo.api
+
+import com.google.gson.annotations.SerializedName
+
+class ArticleSearchObject(
+        val status: String,
+        val response: ArticleSearchResponse
+)
+
+class ArticleSearchResponse(
+        val docs: List<ArticleSearchDoc>
+)
+
+class ArticleSearchDoc(
+        @SerializedName("web_url") val webUrl: String,
+        @SerializedName("pub_date") val pubDate: String,
+        val headline: ArticleSearchHeadline,
+        val multimedia: List<ArticleSearchMultimedia>
+)
+
+class ArticleSearchHeadline(
+        val main: String
+)
+
+class ArticleSearchMultimedia(
+        val type: String,
+        val subtype: String,
+        val url: String
+)
