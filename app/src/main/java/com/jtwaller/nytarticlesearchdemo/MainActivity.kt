46a568b356d1be16080e08dc6e2526de106757e8
Jack Waller <ucdwaller@gmail.com>
2018-12-12 19:39:47 -0800

    Add search view to app bar

--- app/src/main/java/com/jtwaller/nytarticlesearchdemo/MainActivity.kt
+++ app/src/main/java/com/jtwaller/nytarticlesearchdemo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jtwaller.nytarticlesearchdemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import com.jtwaller.nytarticlesearchdemo.api.ArticleSearchObject
 import com.jtwaller.nytarticlesearchdemo.api.NytRestApi
 import io.reactivex.Observer
@@ -19,36 +21,56 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var nytRestApi: NytRestApi
+    lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         NytApp.networkComponent.inject(this)
+    }
 
-        Log.d(TAG, ": created")
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        searchView = menu.findItem(R.id.search_view).actionView as SearchView
+        searchView.queryHint = getString(R.string.search_hint)
 
-        nytRestApi.getArticles("godzilla")
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(object: Observer<ArticleSearchObject> {
-                    override fun onComplete() {
-                        Log.d(TAG, ": complete")
-                    }
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if (p0 == null) return false
 
-                    override fun onSubscribe(d: Disposable) {
-                        Log.d(TAG, ": subbed")
-                    }
+                searchView.clearFocus()
 
-                    override fun onNext(t: ArticleSearchObject) {
-                        Log.d(TAG, ": next")
-                    }
+                nytRestApi.getArticles(p0)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(object: Observer<ArticleSearchObject> {
+                            override fun onComplete() {
+                                Log.d(TAG, ": complete")
+                            }
 
-                    override fun onError(e: Throwable) {
-                        Log.d(TAG, ": error -> " + e.printStackTrace())
-                    }
-                })
+                            override fun onSubscribe(d: Disposable) {
+                                Log.d(TAG, ": subbed")
+                            }
 
+                            override fun onNext(t: ArticleSearchObject) {
+                                Log.d(TAG, ": next")
+                            }
+
+                            override fun onError(e: Throwable) {
+                                Log.d(TAG, ": error -> " + e.printStackTrace())
+                            }
+                        })
+
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
+
+        return true
     }
 
 }
